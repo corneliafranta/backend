ead572530940fc19226d36d6971dde7376a105c9
Cornelia Franta <sf@wh-i.at>
2021-11-25 15:17:27 +0100

    setting up backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3eadec5cc463745effc86fb1591de6d6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ebdcc8e-b324-4117-95f0-a1ad5cf55737'
+};
